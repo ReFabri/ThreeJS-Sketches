0f6585d5c3ac32f70d1f71c706cef52dfc3aad3e
Renato <renato.f.fabri@gmail.com>
2024-07-18 19:38:33 -0300

    Earth mesh initial code

---
+++ earth.js
@@ -0,0 +1,37 @@
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+window.THREE = THREE;
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const geometry = new THREE.IcosahedronGeometry(1, 1);
+const material = new THREE.MeshStandardMaterial({
+  color: 0xffff00,
+  flatShading: true,
+});
+
+const earthMesh = new THREE.Mesh(geometry, material);
+scene.add(earthMesh);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemiLight);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  earthMesh.rotation.x += 0.0001;
+  earthMesh.rotation.y += 0.0001;
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+animate();
